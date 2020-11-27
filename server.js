2c86582ff132f3e486ea2345787d458e2dc7ac9b
stevenaeola <s.p.bradleydurham.ac.uk>
2020-11-27 15:42:03 +0000

    Simple POST request made

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static('client'));
+app.use(express.urlencoded()); //Parse URL-encoded bodies
 
 let things = ['Sue has red hair',
 '40 peer reviewed academic publications',
@@ -13,6 +14,15 @@ let friends =  [ 'Stephen Fry', 'will.i.am'];
  
 app.get('/thing/list', function(req, resp){
   resp.json(things);
-})
+});
+
+app.post('/thing/add', function(req, resp){
+  console.log(req.body);
+  resp.send('Yeah');
+});
+
+app.get('/friend/list', function(req, resp){
+  resp.json(friends);
+});
 
 app.listen(8090);
